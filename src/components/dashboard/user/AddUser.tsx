6a2ec8527caa52a0757f5d22755160649bd91396
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2025-03-04 14:28:26 +0600

    user add

--- src/components/dashboard/user/AddUser.tsx
+++ src/components/dashboard/user/AddUser.tsx
@@ -1,8 +1,88 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
+
+import { Input } from "@/components/share/form";
+import { UserFormData, UserSchema } from "@/schemas/user";
 import { IUserAddProps } from "@/types/user/user.types";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+// import { employeeTypes, genderData } from "@/lib/constants/formData";
 
 export default function AddUser({ setOpen, refetch }: IUserAddProps) {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        // resetField,
+        // control,
+    } = useForm<UserFormData>({
+        resolver: zodResolver(UserSchema),
+    });
+
+    const onSubmit = (data: UserFormData) => {
+        alert(JSON.stringify(data));
+        setIsLoading(true);
+        reset();
+        refetch();
+        setOpen(false);
+        setIsLoading(false);
+    };
+
     return (
-        <div>AddUser</div>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid grid-cols-2 gap-6">
+                    <Input
+                        inputType="text"
+                        labelName="Name"
+                        placeholderText="Enter your name"
+                        name="name"
+                        errors={errors}
+                        register={register}
+                    />
+                    <Input
+                        inputType="email"
+                        labelName="Email"
+                        placeholderText="Enter your email"
+                        name="email"
+                        errors={errors}
+                        register={register}
+                    />
+                    {/* <SelectField
+                        control={control}
+                        name="gender"
+                        data={genderData}
+                        label="Gender"
+                        placeholder="-Select Gender-"
+                        error={errors.gender?.message}
+                        labelKey="label"
+                        valueKey="value"
+                        resetField={resetField}
+                        resetFieldName1=""
+                        resetFieldName2=""
+                        disabledValue="1"
+                        isLoading={false}
+                    />
+                    <RadioField
+                        labelName="Employee Type"
+                        inputName="employeeType"
+                        errorMessage={errors?.employeeType?.message}
+                        register={register}
+                        data={employeeTypes}
+                        gridCols={3}
+                    /> */}
+                </div>
+                <button
+                    type="submit"
+                    disabled={isLoading}
+                    className="bg-[#388E3C] text-bgPrimary text-sm font-medium px-8 py-3 rounded-md mt-6"
+                >
+                    {isLoading ? "Loading..." : "Add"}
+                </button>
+            </form>
+        </div>
     )
 }
